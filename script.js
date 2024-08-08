b73644ef541a17f8fa2ee854d03093cff2db21c6
aisa <aisabihorac965@gmail.com>
2024-08-08 21:02:55 +0200

    Primjer petlje

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
-for(let i=5;i<0;i--){
-    console.log(i);
+//Petlja unazad
+for (let i = 5; i > 0; i--) {
+  console.log(i);
+}
+//Petlja parna
+
+for (let i = 2; i <= 10; i = i + 2) {
+  console.log(i);
 }
